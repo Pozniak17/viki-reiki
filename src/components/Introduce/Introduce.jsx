5290b16ca421c233489da6f833fc7922c853a912
Yevhenii Pozniak <jekamanu@gmail.com>
2025-02-06 14:54:16 +0200

    finished section Introduce: Tablet/Desktop responsive.

--- src/components/Introduce/Introduce.jsx
+++ src/components/Introduce/Introduce.jsx
@@ -5,16 +5,22 @@ import author from "../../../public/images/Introduce/author.jpg";
 export default function Introduce() {
   return (
     <Section>
-      <img src={author} alt="Victoria author" className={styles.image} />
+      <div className={styles.wrapper}>
+        <img src={author} alt="Victoria author" className={styles.image} />
 
-      <h2 className={styles.title}>Viktoria Drachuk</h2>
-      <p className={styles.text}>
-        As a professional Reiki Master, I help to find balance and inner
-        strength, leading people on the path of self-discovery through healing
-        and support.
-      </p>
-      <button className={styles.button_accent}>Schedule now</button>
-      <button className={styles.button}>Service</button>
+        <div>
+          <h2 className={styles.title}>Viktoria Drachuk</h2>
+          <p className={styles.text}>
+            As a professional Reiki Master, I help to find balance and inner
+            strength, leading people on the path of self-discovery through
+            healing and support.
+          </p>
+          <div className={styles.button_wrapper}>
+            <button className={styles.button_accent}>Schedule now</button>
+            <button className={styles.button}>Service</button>
+          </div>
+        </div>
+      </div>
     </Section>
   );
 }
