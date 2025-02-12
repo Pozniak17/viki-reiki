ad7be945884ae9f6a48844a2bf80eaf149fb2756
Marina Nazina <nzyame@gmail.com>
2025-02-12 18:20:35 +0000

    adaptation

--- src/components/Benefits/Benefits.jsx
+++ src/components/Benefits/Benefits.jsx
@@ -1,22 +1,61 @@
+import { useMediaQuery } from 'react-responsive';
 import Icon from '../shared/Icon/Icon';
 import Section from '../shared/Section/Section';
 import Title from '../shared/Title/Title';
 import styles from './Benefits.module.css';
 
 export default function Benefits() {
+	const isMobile = useMediaQuery({ maxWidth: 743 });
+	const isTablet = useMediaQuery({ minWidth: 744, maxWidth: 1043 });
+
+	const backgroundClass = isMobile
+		? styles.benefitsMobile
+		: isTablet
+		? styles.benefitsTablet
+		: styles.benefitsDesktop;
+
+	const circleImageSrc = isMobile
+		? '../../../public/images/Benefits/EllipsePix-mob.jpg'
+		: isTablet
+		? '../../../public/images/Benefits/EllipsePix-tab.jpg'
+		: '../../../public/images/Benefits/EllipsePix-desk.jpg';
+
+	const iconId = isMobile
+		? 'i-vecText-mob'
+		: isTablet
+		? 'i-vecText-tab'
+		: 'i-vecText-desk';
+
+	const sizes = {
+		mobile: { width: '408px', height: '386px' },
+		tablet: { width: '683.844px', height: '640.763px' },
+		desktop: { width: '1115.268px', height: '812.516px' },
+	};
+
+	const iconWidth = isMobile
+		? sizes.mobile.width
+		: isTablet
+		? sizes.tablet.width
+		: sizes.desktop.width;
+	const iconHeight = isMobile
+		? sizes.mobile.height
+		: isTablet
+		? sizes.tablet.height
+		: sizes.desktop.height;
+
 	return (
-		<Section className={styles.benifits}>
+		<Section className={`${styles.benefits} ${backgroundClass}`}>
 			<Title className={styles.title}>Key Benefits</Title>
 			<img
-				src="../../../public/images/Benefits/EllipsePix.jpg"
+				src={circleImageSrc}
 				alt="cirlce portrait"
 				className={styles.circleImage}
 			/>
 			<Icon
 				className={styles.icon}
-				id="i-round-text"
-				width="1115.268px"
-				height="812.516px"
+				id={iconId}
+				width={iconWidth}
+				height={iconHeight}
 			/>
 		</Section>
 	);
