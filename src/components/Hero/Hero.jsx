6cae32b8962d17af367de2ac784c7a0feaf22e20
Yevhenii Pozniak <jekamanu@gmail.com>
2025-02-17 21:14:13 +0200

    bug fixed and add animation in section Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import styles from "./Hero.module.css";
 import leaves from "../../../public/images/Hero/leaves.png";
 import text from "../../../public/images/Hero/text.png";
@@ -7,11 +8,29 @@ export default function Hero() {
     <section className={styles.hero}>
       <img src={text} alt="text" className={styles.hero_cirleText} />
       <div className={styles.hero_circle}>
-        <h1 className={styles.hero_title}>Reiki Master</h1>
-        <h2 className={styles.hero_text}>
+        <motion.h1
+          className={styles.hero_title}
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, delay: 0.4 }}
+        >
+          Reiki Master
+        </motion.h1>
+        <motion.h2
+          className={styles.hero_text}
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, delay: 0.6 }}
+        >
           Take the First Step Toward Your Health and Happiness
-        </h2>
-        <button className={styles.hero_button}>Start Your Journey</button>
+        </motion.h2>
+        <motion.button
+          className={styles.hero_button}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95, originX: 0.5, originY: 0.5 }}
+        >
+          <span>Start Your Journey</span>
+        </motion.button>
         <img src={leaves} alt="leaves" className={styles.hero_imageLeaves} />
       </div>
     </section>
