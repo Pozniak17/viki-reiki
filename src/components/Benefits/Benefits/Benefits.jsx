9aeb554dc169fdff73c1182f0f629999c432a4d9
Yevhenii Pozniak <jekamanu@gmail.com>
2025-02-22 23:19:06 +0200

    finished section Benefits: add responsive imagies 1x 2x and icons

--- src/components/Benefits/Benefits/Benefits.jsx
+++ src/components/Benefits/Benefits/Benefits.jsx
@@ -1,35 +1,86 @@
 import { useMediaQuery } from "react-responsive";
-import { motion, AnimatePresence } from "framer-motion";
-import BenefitsMobile from "../BenefitsMobile/BenefitsMobile";
-import BenefitsTablet from "../BenefitsTablet/BenefitsTablet";
-import BenefitsDesktop from "../BenefitsDesktop/BenefitsDesktop";
+// import { motion, AnimatePresence } from "framer-motion";
 import styles from "./Benefits.module.css";
 
 export default function Benefits() {
   const isMobileScreen = useMediaQuery({ maxWidth: 743 });
   const isTabletScreen = useMediaQuery({ minWidth: 744, maxWidth: 1439 });
-  // const isDesktopScreen = useMediaQuery({ minWidth: 1440 });
-
-  const BenefitsComponent = isMobileScreen
-    ? BenefitsMobile
-    : isTabletScreen
-    ? BenefitsTablet
-    : BenefitsDesktop;
+  const isDesktopScreen = useMediaQuery({ minWidth: 1440 });
 
   return (
     <section className={styles.benefits}>
       <h2 className={styles.title}>Key Benefits</h2>
-      <AnimatePresence mode="wait">
-        <motion.div
-          key={BenefitsComponent.name} // Унікальний ключ для анімації
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -20 }}
-          transition={{ duration: 0.3 }}
-        >
-          <BenefitsComponent />
-        </motion.div>
-      </AnimatePresence>
+
+      <div className={styles.wrapper}>
+        {isMobileScreen && (
+          <img
+            src="/public/icons/Benefits/Mobile/Text-mobile.svg"
+            alt="Text"
+            className={styles.text_icon}
+          />
+        )}
+
+        {isTabletScreen && (
+          <img
+            src="/public/icons/Benefits/Tablet/Text-tablet.svg"
+            alt="Text"
+            className={styles.text_icon}
+          />
+        )}
+
+        {isDesktopScreen && (
+          <img
+            src="/public/icons/Benefits/Desktop/Text-desktop.svg"
+            alt="Text"
+            className={styles.text_icon}
+          />
+        )}
+
+        <picture>
+          {/* Десктоп */}
+          <source
+            srcSet="/images/Benefits/Desktop/Ellipse-desktop-1x.png 1x, /images/Benefits/Desktop/Ellipse-desktop-2x.png 2x"
+            media="(min-width: 1440px)"
+          />
+
+          {/* Таблет */}
+          <source
+            srcSet="/images/Benefits/Tablet/Ellipse-tablet-1x.png 1x, /images/Benefits/Tablet/Ellipse-tablet-2x.png 2x"
+            media="(min-width: 744px)"
+          />
+          {/* Мобільні */}
+          <source
+            srcSet="/images/Benefits/Mobile/Ellipse-mobile-1x.png 1x, /images/Benefits/Mobile/Ellipse-mobile-2x.png 2x"
+            media="(max-width: 743px)"
+          />
+          {/* Дефолт*/}
+          <img
+            src="/images/Benefits/Mobile/Ellipse-mobile-1x.png"
+            alt="Meditation"
+            className={styles.meditation_image}
+          />
+        </picture>
+      </div>
     </section>
   );
 }
+
+// export default function Benefits() {
+//   const screenType = useMediaQuery({ maxWidth: 743 })
+//     ? "Mobile"
+//     : useMediaQuery({ minWidth: 744, maxWidth: 1439 })
+//     ? "Tablet"
+//     : "Desktop";
+
+//   const imageSrc = `/public/icons/Benefits/${screenType}/Text-${screenType.toLowerCase()}.svg`;
+
+//   return (
+//     <section className={styles.benefits}>
+//       <h2 className={styles.title}>Key Benefits</h2>
+
+//       <div className={styles.wrapper}>
+//         <img src={imageSrc} alt="Text" className={styles.text_icon} />
+//       </div>
+//     </section>
+//   );
+// }
