0c7aaad07662ee20137e5d4e2f0549121fce9138
Yevhenii Pozniak <jekamanu@gmail.com>
2025-02-28 13:19:19 +0200

    add 1x 2x to Introduce section

--- src/components/Introduce/Introduce.jsx
+++ src/components/Introduce/Introduce.jsx
@@ -1,6 +1,5 @@
 import Section from "../shared/Section/Section";
 import styles from "./Introduce.module.css";
-import author from "../../../public/images/Introduce/author.jpg";
 import { useTranslation } from "react-i18next";
 
 export default function Introduce() {
@@ -8,7 +7,31 @@ export default function Introduce() {
   return (
     <Section>
       <div className={styles.wrapper}>
-        <img src={author} alt="Victoria author" className={styles.image} />
+        <picture>
+          {/* Десктоп */}
+          <source
+            srcSet="/images/Introduce/Desktop/Introduce-desktop-1x.jpg 1x, /images/Introduce/Desktop/Introduce-desktop-2x.jpg 2x"
+            media="(min-width: 1440px)"
+          />
+
+          {/* Таблет */}
+          <source
+            srcSet="/images/Introduce/Tablet/Introduce-tablet-1x.jpg 1x, /images/Introduce/Tablet/Introduce-tablet-2x.jpg 2x"
+            media="(min-width: 744px)"
+            sizes="100vw"
+          />
+          {/* Мобільні */}
+          <source
+            srcSet="/images/Introduce/Mobile/Introduce-mobile-1x.jpg 1x, /images/Introduce/Mobile/Introduce-mobile-2x.jpg 2x"
+            media="(max-width: 743px)"
+          />
+          {/* Дефолт*/}
+          <img
+            src="/images/Introduce/Desktop/Introduce-desktop-1x.jpg"
+            alt="Victoria"
+            className={styles.image}
+          />
+        </picture>
 
         <div>
           <h2 className={styles.title}>{t("introduce.introduceTitle")}</h2>
