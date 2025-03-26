8fb86e490a7a2be316f21b73ea7aae1618bdd485
Yevhenii Pozniak <jekamanu@gmail.com>
2025-03-26 17:09:45 +0200

    update

--- src/components/Services/ServicesCard/ServicesCard.jsx
+++ src/components/Services/ServicesCard/ServicesCard.jsx
@@ -1,7 +1,10 @@
 import { useTranslation } from "react-i18next";
 import styles from "./ServicesCard.module.css";
+import { useState } from "react";
+import CustomModal from "../../CustomModal/CustomModal";
 
 export default function ServicesCard({ image, title, text }) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const { t, i18n } = useTranslation();
   const isUkrainian = i18n.language === "ua"; //чи мова українська
   return (
@@ -10,12 +13,14 @@ export default function ServicesCard({ image, title, text }) {
         <img src={image} alt="author photo" className={styles.image} />
         <div className={styles.card_wrapper}>
           <h3 className={styles.card_title}>{title}</h3>
-          {/* <span className={styles.card_price}>60 €</span> */}
         </div>
         <p className={styles.text}>{text}</p>
       </div>
 
-      <button className={styles.button}>Open to read</button>
+      <button className={styles.button} onClick={() => setModalIsOpen(true)}>
+        {t("services.button")}
+      </button>
+      <CustomModal isOpen={modalIsOpen} onClose={() => setModalIsOpen(false)} />
     </div>
   );
 }
